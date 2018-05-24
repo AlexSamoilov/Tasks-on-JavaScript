0a96c4802f9422adf6d53d8587ffb8a9aa06ee59
Alexandr <Gemini_1982@mail.ru>
2018-05-24 22:44:53 +0300

    part of JS lesson 7

--- JS_lesson7.js
+++ JS_lesson7.js
@@ -5,17 +5,35 @@
 // var str = 'JS';
 // alert(str.toLowerCase());
 // Дана строка 'я учу javascript!'. Найдите количество символов в этой строке. 
-
-
-
-
-
-
-
-
-
-
-
+// var str = 'я учу javascript!';
+// alert(str.length);
+// Дана строка 'я учу javascript!'. Вырежите из нее слово 'учу' и слово 'javascript' тремя разными способами (через substr, substring, slice).
+// var str = 'я учу javascript!';
+// alert(str.substr(2, 3) + ' ' + str.substr(6, 10));
+// alert(str.substring(2, 5) + ' ' + str.substring(6, 16));
+// alert(str.slice(2, 6) + ' ' + str.slice(6, 16));
+// Дана строка 'я учу javascript!'. Найдите позицию подстроки 'учу'.
+// var str = 'я учу javascript!';
+// alert(str.indexOf('учу'));
+// Дана переменная str, в которой хранится какой-либо текст. Реализуйте обрезание длинного текста по следующему принципу: если количество символов этого текста больше заданного в переменной n, то в переменную result запишем первые n символов строки str и добавим в конец троеточие '...'. В противном случае в переменную result запишем содержимое переменной str.
+// var str = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, quam!';
+// var result;
+// var n = 17;
+// if (n < str.length) {
+// 	result = str.substr(0, n-1) + ' ...';
+// } else {
+// 	result = str;
+// };
+// alert(result);
+// Дана строка 'Я-учу-javascript!'. Замените все дефисы на '!' с помощью глобального поиска и замены.
+// var str = 'Я-учу-javascript!';
+// var newStr = str.replace(/-/g, '!');
+// alert(newStr);
+// Дана строка 'я учу javascript!'. С помощью метода split запишите каждое слово этой строки в отдельный элемент массива.
+// var str = 'я учу javascript!';
+// var arr = str.split(' ');
+// alert(arr);
+// В переменной date лежит дата в формате '2025-12-31'. Преобразуйте эту дату в формат '31.12.2025'.
 
 
 
