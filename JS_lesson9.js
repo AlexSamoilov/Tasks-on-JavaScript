c874630f12acea7956d42c25e73e75b93e3181c5
Alexandr <Gemini_1982@mail.ru>
2018-05-28 23:21:21 +0300

    JS lesson 9

---
+++ JS_lesson9.js
@@ -0,0 +1,36 @@
+// Дана строка. Сделайте заглавным первый символ этой строки не используя цикл. Найдите два решения.
+// var str = 'hello';
+// str = str[0].toUpperCase() + str.substr(1);
+// alert(str);
+// ...
+// var str = 'hello';
+// str = str.split('');
+// str[0] = str[0].toUpperCase();
+// var result = str.join('');
+// alert(result);
+// Дана строка, например, '123456'. Переверните эту строку (сделайте из нее '654321') не используя цикл.
+// var str = '123456';
+// arr = str.split('');
+// arr = arr.reverse();
+// var result = arr.join('');
+// alert(result);
+// Дано число, например, 3751. Отсортируйте цифры в нем (сделайте из него 1357) не используя цикл.
+// var num = '3751';
+// arr = num.split('');
+// arr.sort();
+// var result = arr.join('');
+// alert(result);
+// Проверьте, что строка начинается на http://.
+// var str = 'http://phphtml';
+// if (str.substr(0, 7) == 'http://') {
+// 	alert('Да');
+// } else {
+// 	alert('Нет');
+// }
+// Проверьте, что строка заканчивается на .html.
+// var str = 'index.html';
+// if (str.substr(-5) == '.html') {
+// 	alert('Да');
+// } else {
+// 	alert('Нет');
+// }
