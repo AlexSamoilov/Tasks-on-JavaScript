bbef29a87750f82f1004fe5646afb662055b5b95
AlexSamoilov <Gemini_1982@mail.ru>
2018-06-05 09:49:12 +0300

    JS lesson 23

---
+++ JS_lesson23.js
@@ -0,0 +1,46 @@
+// Дан элемент #elem. Добавьте ему класс www.
+// var elem = document.getElementById('elem');
+// elem.classList.add('www');
+// Дан элемент #elem. Удалите у него класс www.
+// var elem = document.getElementById('elem');
+// elem.classList.remove('www');
+// Дан элемент #elem. Проверьте наличие у него класса www.
+// var elem = document.getElementById('elem');
+// var checkClass = elem.classList.contains('www');
+// alert(checkClass);
+// Дан элемент #elem. Добавьте ему класс www, если его нет и удалите - если есть.
+// var elem = document.getElementById('elem');
+// elem.classList.toggle('www');
+// Дан элемент #elem. Узнайте количество его классов.
+// var elem = document.getElementById('elem');
+// var length = elem.classList.length;
+// alert(length);
+// Дан элемент #elem. Выведите последовательно алертом его классы.
+// var elem = document.getElementById('elem');
+// var classNames = elem.classList;
+// for (var i = 0; i < classNames.length; i++) {
+// 	alert(classNames[i]);
+// }
+// Дан элемент #elem. Сделайте его красного цвета, размером 30px, добавьте ему границу. Решите задачу с помощью свойства cssText.
+// var elem = document.getElementById('elem');
+// elem.style.cssText = 'color: red; font-size: 30px; border: 1px solid black;';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
