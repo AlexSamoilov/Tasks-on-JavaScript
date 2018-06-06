ce3e671863c172926e665ee22ac173ddb0608bd9
Alexandr <Gemini_1982@mail.ru>
2018-06-06 21:31:09 +0300

    JS lesson 23

--- JS_lesson23.js
+++ JS_lesson23.js
@@ -64,5 +64,23 @@
 // function func() {
 // 	alert(this.innerHTML);
 // }
+Даны 3 инпута и кнопка. По нажатию на кнопку получите числа, стоящие в этих инпутах и запишите их сумму в четвертый инпут.
+var elem = document.getElementById('task');
+elem.addEventListener('click', func);
+var elem1 = document.getElementsByClassName('cls');
+
+function func() {
+	var sum = 0;
+	for (var i = 0; i < elem1.length; i++) {
+		sum += +elem1[i].value;
+	}
+	var newElem = document.getElementById('sum');
+	newElem.value = sum;
+}    
+
+
+
+
+
 
 
