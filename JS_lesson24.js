2d1e7a9b2e359366c0fe29603173749ea5409605
AlexSamoilov <Gemini_1982@mail.ru>
2018-06-07 09:08:52 +0300

    JS lesson 24

---
+++ JS_lesson24.js
@@ -0,0 +1,9 @@
+// Дан элемент ul, а в нем li #elem. Вставьте перед элементом #elem новую li с текстом '!!!'.
+// var ul = document.getElementById('ul');
+// var test = document.getElementById('elem');
+
+// var li = document.createElement('li');
+// li.innerHTML = '!!!';
+
+// ul.insertBefore(li, test);
+
