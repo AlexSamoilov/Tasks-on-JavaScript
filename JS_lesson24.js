f93c4c9597cbc92b21e1163b4749de635a941c77
AlexSamoilov <Gemini_1982@mail.ru>
2018-06-11 09:11:42 +0300

    JS lesson 24 mid

--- JS_lesson24.js
+++ JS_lesson24.js
@@ -27,3 +27,19 @@
 // var elem = document.getElementById('elem');
 // elem.insertAdjacentHTML('beforeEnd','<span>!!!</span>');
 
+// Дан элемент #elem. Найдите первого потомка этого элемента и сделайте его текст красного цвета.
+
+// var elem = document.getElementById('elem');
+// elem.firstElementChild.style.color = 'red'; 
+
+// Дан элемент #elem. Найдите последнего потомка этого элемента и сделайте его текст красного цвета.
+
+// var elem = document.getElementById('elem');
+// elem.lastElementChild.style.color = 'red';
+
+// Дан элемент #elem. Найдите всех потомков этого элемента и добавьте им в конец текст '!'.
+
+// var elem = document.getElementById('elem');
+// for (var i = 0; i < elem.children.length; i++) {
+// 	elem.children[i].innerHTML += ' !';
+// }
