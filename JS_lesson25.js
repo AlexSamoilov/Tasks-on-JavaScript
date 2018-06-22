875627b0d03e1e4241d85166ac940ee02da6d560
Alexandr <Gemini_1982@mail.ru>
2018-06-22 20:54:36 +0300

    lesson 25 (4)

---
+++ JS_lesson25.js
@@ -0,0 +1,36 @@
+// Дан элемент #elem с границами. По нажатию на кнопку выведите толщину его верхней границы.
+// var elem = document.getElementById('elem');
+// var button = document.getElementById('button');
+// button.addEventListener('click', func);
+
+// function func() {
+// 	alert(elem.clientTop);
+// }
+// Дан элемент #elem с границами. По нажатию на кнопку выведите толщину его левой границы.
+// var elem = document.getElementById('elem');
+// var button = document.getElementById('button');
+// button.addEventListener('click', func);
+
+// function func() {
+// 	alert(elem.clientLeft);
+// }
+// Дан элемент #elem с границами. По нажатию на кнопку выведите его полную ширину с учетом границы и padding. 
+// var elem = document.getElementById('elem');
+// var button = document.getElementById('button');
+// button.addEventListener('click', func);
+
+// function func() {
+// 	alert(elem.offsetWidth);
+// }
+// Дан элемент #elem с границами. По нажатию на кнопку выведите его полную высоту с учетом границы и padding.
+// var elem = document.getElementById('elem');
+// var button = document.getElementById('button');
+// button.addEventListener('click', func);
+
+// function func() {
+// 	alert(elem.offsetHeight);
+// }
+
+
+
+
