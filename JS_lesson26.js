b7bf10dfebaacc779506e09ce281b43d626039fe
Alexandr <Gemini_1982@mail.ru>
2018-07-14 11:29:51 +0300

    lesson 26

---
+++ JS_lesson26.js
@@ -0,0 +1,19 @@
+// Дан ul, в нем несколько li. Под ul сделайте кнопку, по нажатию на которую в конец ul будет добавляться новый li с текстом 'пункт'. Сделайте так, чтобы при клике на каждый li, ему в конец добавлялся '!'. Это должно работать и для вновь добавленных li. Задачу решите с помощью делегирования (то есть событие должно быть навешано на ul).
+
+// var ul = document.getElementById('ul');
+// ul.addEventListener('click', addSign);
+// function addSign() {
+// 	event.target.innerHTML = event.target.innerHTML + '!';
+// }
+
+// var button = document.getElementById('btn');
+// button.addEventListener('click', addLi);
+
+// function addLi() {
+// 	var li = document.createElement('li');
+// 	li.innerHTML = 'пункт';
+// 	ul.appendChild(li);
+// }
+
+
+
