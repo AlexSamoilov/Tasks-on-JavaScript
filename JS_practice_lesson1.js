c849e40185eda862de1eda97902218f45731afa8
Alexandr <Gemini_1982@mail.ru>
2018-07-19 09:33:37 +0300

    practice - lesson 1(4)

--- JS_practice_lesson1.js
+++ JS_practice_lesson1.js
@@ -39,6 +39,28 @@
 // 	var newElem = document.getElementById('result');
 // 	newElem.innerHTML = sum;
 // }    
+// Дан инпут. В него вводятся числа через запятую. По потери фокуса найдите среднее арифметическое этих чисел (сумма делить на количество).
+
+// var elem = document.getElementById('num');
+// elem.addEventListener('blur', func);
+
+// function func() {
+// 	var sum = 0;
+// 	var str = elem.value;
+// 	var arr = str.split(',');
+//   	for (var i = 0; i < arr.length; i++) {
+// 		sum += +arr[i];
+// 	}
+// 	var newElem = document.getElementById('result');
+// 	newElem.innerHTML = sum/arr.length;
+// }    
+
+
+
+
+
+
+
 
 
 
