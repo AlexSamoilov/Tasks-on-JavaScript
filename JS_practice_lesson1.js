09b7e49fb7a2ebc8d4c5f409e95b3c10ff1473d8
Alexandr <Gemini_1982@mail.ru>
2018-07-20 20:22:46 +0300

    practice - lesson 1(5)

--- JS_practice_lesson1.js
+++ JS_practice_lesson1.js
@@ -55,6 +55,36 @@
 // 	newElem.innerHTML = sum/arr.length;
 // }    
 
+//  Дан инпут. В него вводится ФИО через пробел. По потери фокуса запишите фамилию, имя и отчество в отдельные инпуты.
+// var elem = document.getElementById('input');
+// elem.addEventListener('focus', func);
+// elem.addEventListener('blur', func2);
+  
+// function func() {
+// 	elem.placeholder = '';
+// }
+
+// function func2() {
+// 	var elem = document.getElementById('input');
+// 	var elems = document.getElementsByClassName('name');
+// 	if (elem.value !== 0) {
+// 		var str = elem.value;
+// 		var arr = str.split(' ');
+// 		for (var i = 0; i < arr.length; i++) {
+// 			elems[i].value = arr[i];
+// 		}
+// 	}
+// 	if (elem.value == 0) {
+// 		elem.placeholder = 'Введите Ваши ФИО';
+// 		for (var i = 0; i < elems.length; i++) {
+// 			elems[i].value = '';
+// 		}
+// 	}
+// }       
+
+
+
+
 
 
 
