b1529552855694c18516e1c90f10401a27de84e9
Alexandr <Gemini_1982@mail.ru>
2018-08-07 20:51:18 +0300

    practice - lesson 1(24)

--- JS_practice_lesson2.js
+++ JS_practice_lesson2.js
@@ -1,13 +1,29 @@
-Дан инпут. В него вводится число. По потери фокуса проверьте, что в нем лежит число от 1 до 100. Если это так - покрасьте инпут в зеленый цвет, а если не так - в красный.
-var inp = document.querySelector('input');
-inp.addEventListener('blur', func);
+// Дан инпут. В него вводится число. По потери фокуса проверьте, что в нем лежит число от 1 до 100. Если это так - покрасьте инпут в зеленый цвет, а если не так - в красный.
+// var inp = document.querySelector('input');
+// inp.addEventListener('blur', func);
 
-function func() {
-	var content = inp.value;
-	if (content > 0 && content < 101){
-		inp.style.background = 'green';
-	} else {
-		inp.style.background = 'red';
-	}
-} 
+// function func() {
+// 	var content = inp.value;
+// 	if (content > 0 && content < 101){
+// 		inp.style.background = 'green';
+// 	} else {
+// 		inp.style.background = 'red';
+// 	}
+// } 
+
+// Дан инпут. Выделите любой текст на странице. По окончанию выделения этот текст должен записаться в этот инпут.
+// var inp = document.querySelector('input');
+// document.documentElement.addEventListener('mouseup', func);
+
+// function func() {
+// 	var content = window.getSelection().toString();
+// 	inp.value = content;
+// }   
+
+
+
+
+
+
+
 
