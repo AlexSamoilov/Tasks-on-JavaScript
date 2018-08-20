01470ff3414a248cc51afd6c70dacc6359fc5912
Alexandr <Gemini_1982@mail.ru>
2018-08-20 10:20:50 +0300

    practice - lesson 3(36)

---
+++ JS_practice_lesson3.js
@@ -0,0 +1,19 @@
+// Дана ссылка. Дан чекбокс. По нажатию на ссылку меняйте состояние чекбокса с отмеченного на неотмеченное и наоборот. 
+// function func() {
+// 	var elem = document.getElementById('text');
+// 	if (elem.checked) {
+// 		elem.checked = false;
+//  	} else {
+// 		elem.checked = true;
+//  	}
+//   }    
+
+
+
+
+
+
+
+
+
+
